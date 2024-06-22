adcc7b8814da1927954709891d3c24b1a84f6ed3
wakoIniad <s1f102400606@iniad.org>
2024-06-22 22:52:08 +0900

    homeのfontの変更

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-$(".game-preview").outerHeight($(".game-preview").width()*1.5);
-console.log($(".game-preview").width())
-
-$( window ).on( "resize", ()=> {
+//$( window ).on("load", ()=>{
     $(".game-preview").outerHeight($(".game-preview").width()*1.5);
     console.log($(".game-preview").width())
-})
+
+    $( window ).on( "resize", ()=> {
+        $(".game-preview").outerHeight($(".game-preview").width()*1.5);
+        console.log($(".game-preview").width())
+    })
+//});
