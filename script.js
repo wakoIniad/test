9e8f3d1c345921bb076c9514436a0a32f6380560
wakoIniad <s1f102400606@iniad.org>
2024-06-22 21:40:31 +0900

    途中経過２

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//$(".game-preview").outerHeight($(".game-preview").width*1.5);
+$(".game-preview").outerHeight($(".game-preview").width()*1.5);
+console.log($(".game-preview").width())
+
+$( window ).on( "resize", ()=> {
+    $(".game-preview").outerHeight($(".game-preview").width()*1.5);
+    console.log($(".game-preview").width())
+})
